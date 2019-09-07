0f0ab4cb4ed59b8c492694d29c39d0816b0f298b
miss2483466793 <55003446+miss2483466793@users.noreply.github.com>
2019-09-07 18:48:30 -0400

    Add files via upload

---
+++ Lennon.py
@@ -0,0 +1,28 @@
+from flask import Flask
+app = Flask('app')
+
+@app.route('/')
+def main_screen(name="lennon"):
+  return(name)
+app.run(host='0.0.0.0', port=8080)
+
+def output():
+try:
+  #if
+except:
+  print("An exception occurred")
+
+"""
+lis=[]
+def find
+dict={}
+open("demofile.txt","r")
+import os
+os.remove
+import mysql.connector
+#input
+#import daytime
+#time=datetime.datetime.now()
+tag_input=input()
+#import
+"""
