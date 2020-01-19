a0088b13f32e25345a822b4acbf3f807ff93dd26
miss2483466793 <55003446+miss2483466793@users.noreply.github.com>
2020-01-20 03:10:16 +0800

    Add files via upload

--- Lennon.py
+++ Lennon.py
@@ -1,28 +1,32 @@
-from flask import Flask
-app = Flask('app')
-
-@app.route('/')
-def main_screen(name="lennon"):
-  return(name)
-app.run(host='0.0.0.0', port=8080)
-
-def output():
+"""
 try:
   #if
 except:
   print("An exception occurred")
 
+from datetime import datetime
+datetime.date()
+import timeit
+timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
 """
-lis=[]
-def find
+
+name=["連儂","Lennon"]
+solgan=["We Connect",]
+print(name,solgan)
+
+#imformation input
+def output():
+    return()
+name=input()
+tag=input()
+
+"""
+sticky notes
+temp.emotionflow.com/s/
 dict={}
 open("demofile.txt","r")
 import os
 os.remove
 import mysql.connector
-#input
-#import daytime
 #time=datetime.datetime.now()
-tag_input=input()
-#import
 """
